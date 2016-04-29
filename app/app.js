af51195acb0a140c44ba34a0fa3768e4d4ca507b
aileen santos <asantos3026@gmail.com>
2016-04-29 12:36:26 -0700

    wired tabs and functionality for each tab

--- app/app.js
+++ app/app.js
@@ -29,6 +29,7 @@ var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'my
     name: "Pentagonal Gem",
     price: 25.95,
     description: "Madiera citrine cut in a pentagonal barion style has fine golden orange color and Vs clarity. This gem cut by Larry Moriarty weighs 9.31 carats and has exceptional brilliance. Mined in Uraguay.",
+    shine: 8,
     images: [
       'images/pentagonal-3.jpg'
       ],
@@ -38,6 +39,7 @@ var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'my
     name: "Opal",
     price: 25.95,
     description: "is a gem-quality form of hydrated amorphous silicon dioxide. Its name is derived from the Sanskrit word for 'stone'. It is gemologically classed as a mineraloid rather than a mineral, owing to its amorphous form.",
+    shine: 8,
     images: [
      'images/opal-gem.jpg'
       ],
@@ -47,6 +49,7 @@ var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'my
     name: "Dodecahedron Gem",
     price: 35.95,
     description: "has been a source of metaphysical interest for at least 2000 years. Like a crystal or gem, its facets and symmetries compel our eyes and hearts to observe life more deeply. Some have believed that the Dodecahedron represents an idealized form of Divine thought, will, or idea. ",
+    shine: 8,
     images: [
      'images/dodecahedron-full.jpg'
       ],
