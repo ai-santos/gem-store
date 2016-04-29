42bfda93e95b413b6eea9c99a8fd0c3ee2fd8dbd
aileen santos <asantos3026@gmail.com>
2016-04-29 08:16:31 -0700

    pictures are showing up on the page now

--- app/app.js
+++ app/app.js
@@ -4,90 +4,51 @@
 var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'myApp.version'])
 
   var gems = [
-  {
-    name: "Amethyst",
-    price: 20,
-    description: "is a meditative and calming stone which works in the emotional, spiritual, and physical planes to provide calm, balance, patience, and peace.",
-    images: {
-      full: "app/images/amethyst.jpg",
-      thumbnail: "app/images/Amethyst-2.jpg"
-    },
+{
+    name: 'Dodecahedron',
+    price: 2,
+    description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    images: [
+      {
+        full: 'images/dodecahedron-full.jpg',
+        thumbnail: 'images/gem-01.gif'
+      },
+      {
+        full: 'images/pentagonal-gem.jpg',
+        thumbnail: 'images/pentagonal-gem.jpg'
+      }
+    ],
     reviews: [
       {
-        "stars": 5,
-        "body": "Makes me feel like royalty, truly beautiful.",
-        "author": "ai@leen.com"
+        stars: 5,
+        body: "Makes me feel like royalty, truly beautiful.",
+        author: "ai@leen.com"
       },
       {
-        "stars": 1,
-        "body": "Does not look like the picture, blech.",
-        "author": "lee@hates.com"
+        stars: 1,
+        body: "Does not look like the picture, blech.",
+        author: "lee@hates.com"
       }
-    ]
-  },
-  {
-    name: "Dodecahedron",
-    price: 25,
-    description: "has been a source of metaphysical interest for at least 2000 years. Like a crystal or gem, its facets and symmetries compel our eyes and hearts to observe life more deeply. Some have believed that the Dodecahedron represents an idealized form of Divine thought, will, or idea. ",
-    images: {
-      full: "app/images/dodecahedron-full-2.jpg",
-      thumbnail: "app/images/dodecahedron-full.jpg"
-    },
-    "reviews": [
-      {
-        "stars": 5,
-        "body": "Makes me feel like royalty, truly beautiful.",
-        "author": "ai@leen.com"
-      },
-      {
-        "stars": 1,
-        "body": "Does not look like the picture, blech.",
-        "author": "lee@hates.com"
-      }
-    ]
-  },
-  {
-    "name": "Pentagonal",
-    "description": "Madiera citrine cut in a pentagonal barion style has fine golden orange color and Vs clarity. This gem cut by Larry Moriarty weighs 9.31 carats and has exceptional brilliance. Mined in Uraguay.",
-    "images": [
-      "images/pentagonal-gem.jpg",
-      "images/pentagonal-2.jpg",
-      "images/pentagonal-3.JPG"
     ],
-    "reviews": [
+    canPurchase: true
+},
+{
+    name: "Pentagonal Gem",
+    price: 5.95,
+    description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    images: [
       {
-        "stars": 5,
-        "body": "Makes me feel like royalty, truly beautiful.",
-        "author": "ai@leen.com"
+        full: 'images/dodecahedron-full.jpg',
+        thumbnail: 'images/gem-01.gif'
       },
       {
-        "stars": 1,
-        "body": "Does not look like the picture, blech.",
-        "author": "lee@hates.com"
+        full: 'images/dodecahedron-full-2.jpg',
+        thumbnail: 'images/dodecahedron-thumb-2.gif'
       }
-    ]
-  },
-  {
-    "name": "Opal",
-    "description": "is a gem-quality form of hydrated amorphous silicon dioxide. Its name is derived from the Sanskrit word for 'stone'. It is gemologically classed as a mineraloid rather than a mineral, owing to its amorphous form.",
-    "images": [
-      "images/opal-gem.jpg",
-      "images/opal-2.jpg",
-      "images/opal-3.png"
-    ],
-    "reviews": [
-      {
-        "stars": 5,
-        "body": "Makes me feel like royalty, truly beautiful.",
-        "author": "ai@leen.com"
-      },
-      {
-        "stars": 1,
-        "body": "Does not look like the picture, blech.",
-        "author": "lee@hates.com"
-      }
-    ]
-  }
+      ],
+    canPurchase: true
+}
+
 ];
 
   app.controller('StoreController', function(){
