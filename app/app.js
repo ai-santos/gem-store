74fb88f3a239653110f8c567a8bfbeb719c5505e
aileen santos <asantos3026@gmail.com>
2016-04-29 12:06:13 -0700

    centered and looped through images via directives

--- app/app.js
+++ app/app.js
@@ -9,15 +9,8 @@ var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'my
     price: 25,
     description: "is a meditative and calming stone which works in the emotional, spiritual, and physical planes to provide calm, balance, patience, and peace.",
     images: [
-      {
-        full: 'images/Amethyst-3.jpg',
-        thumbnail: 'images/amethyst.gif'
-      },
-      {
-        full: 'images/Amethyst-2.jpg',
-        thumbnail: 'images/amethyst.jpg'
-      }
-    ],
+      'images/Amethyst-3.jpg'
+      ],
     reviews: [
       {
         stars: 5,
@@ -37,14 +30,7 @@ var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'my
     price: 25.95,
     description: "Madiera citrine cut in a pentagonal barion style has fine golden orange color and Vs clarity. This gem cut by Larry Moriarty weighs 9.31 carats and has exceptional brilliance. Mined in Uraguay.",
     images: [
-      {
-        full: 'images/pentagonal-3.jpg',
-        thumbnail: 'images/gem-01.gif'
-      },
-      {
-        full: 'images/dodecahedron-full-2.jpg',
-        thumbnail: 'images/dodecahedron-thumb-2.gif'
-      }
+      'images/pentagonal-3.jpg'
       ],
     canPurchase: true
 },
@@ -53,14 +39,7 @@ var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'my
     price: 25.95,
     description: "is a gem-quality form of hydrated amorphous silicon dioxide. Its name is derived from the Sanskrit word for 'stone'. It is gemologically classed as a mineraloid rather than a mineral, owing to its amorphous form.",
     images: [
-      {
-        full: 'images/opal-gem.jpg',
-        thumbnail: 'images/gem-01.gif'
-      },
-      {
-        full: 'images/dodecahedron-full-2.jpg',
-        thumbnail: 'images/dodecahedron-thumb-2.gif'
-      }
+     'images/opal-gem.jpg'
       ],
     canPurchase: true
 },
@@ -69,14 +48,7 @@ var app = angular.module('gemStore', ['ngRoute','myApp.view1','myApp.view2', 'my
     price: 35.95,
     description: "has been a source of metaphysical interest for at least 2000 years. Like a crystal or gem, its facets and symmetries compel our eyes and hearts to observe life more deeply. Some have believed that the Dodecahedron represents an idealized form of Divine thought, will, or idea. ",
     images: [
-      {
-        full: 'images/dodecahedron-full.jpg',
-        thumbnail: 'images/dodecahedron-thumb-2.gif'
-      },
-      {
-        full: 'images/dodecahedron-full-2.jpg',
-        thumbnail: 'images/dodecahedron-thumb-2.gif'
-      }
+     'images/dodecahedron-full.jpg'
       ],
     canPurchase: true
 }
